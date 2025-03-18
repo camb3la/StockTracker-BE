bc1b0e6beba41564a3d12a5fdfbc815f2f98e466
camb3la <59169021+camb3la@users.noreply.github.com>
2025-03-18 21:59:32 +0100

    Inzio Progetto Back End

---
+++ src/main/java/com/example/StockTracker/BE/StockTrackerBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.StockTracker.BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockTrackerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockTrackerBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/StockTracker/BE/StockTrackerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StockTracker.BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockTrackerBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
