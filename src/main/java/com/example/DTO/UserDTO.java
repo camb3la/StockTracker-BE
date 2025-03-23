613d08ecab6c4eb4ad967fd205c7a054b99850f1
camb3la <59169021+camb3la@users.noreply.github.com>
2025-03-23 19:05:24 +0100

    Rimossi firstName e lastName da UserDto

--- src/main/java/com/example/DTO/UserDTO.java
+++ src/main/java/com/example/DTO/UserDTO.java
@@ -16,8 +16,6 @@ public class UserDTO {
     private Long id;
     private String username;
     private String email;
-    private String firstName;
-    private String lastName;
 
     public static UserDTO fromUser(User user) {
         return UserDTO.builder()
