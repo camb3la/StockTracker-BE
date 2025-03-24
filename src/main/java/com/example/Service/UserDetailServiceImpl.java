37b11ce34ab9ebc7066409f3278f2e38340d9f8d
camb3la <59169021+camb3la@users.noreply.github.com>
2025-03-24 11:25:18 +0100

    Implementato UserDetailServiceImpl

---
+++ src/main/java/com/example/Service/UserDetailServiceImpl.java
@@ -0,0 +1,34 @@
+package com.example.Service;
+
+import com.example.Model.User;
+import com.example.Repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailServiceImpl implements UserDetailService{
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Utente non trovato con Username " + username));
+
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"))
+        );
+    }
+
+}
