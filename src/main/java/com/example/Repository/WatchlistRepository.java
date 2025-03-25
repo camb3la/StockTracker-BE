7267d556ba09f3e3f783d40715af97fb67d1c6c3
camb3la <59169021+camb3la@users.noreply.github.com>
2025-03-25 21:36:00 +0100

    Modificato getStocksFromWatchList in WatchlistRepository

--- src/main/java/com/example/Repository/WatchlistRepository.java
+++ src/main/java/com/example/Repository/WatchlistRepository.java
@@ -1,9 +1,10 @@
 package com.example.Repository;
 
-import com.example.Model.Stock;
 import com.example.Model.User;
 import com.example.Model.Watchlist;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -18,8 +19,17 @@ public interface WatchlistRepository extends JpaRepository<Watchlist, Long> {
 
     Optional<Watchlist> findByIdAndUserId(Long id, Long userId);
 
-    boolean existByNameAndUserId(String name, Long userId);
+    boolean existsByNameAndUserId(String name, Long userId);
 
-    List<Stock> getStocksFromWatchList(Long id, Long userId);
+    @Query("SELECT s FROM Stock s " +
+            "JOIN s.watchlists w " +
+            "WHERE w.id = :watchlistId " +
+            "AND w.user.id = :userId " +
+            "AND s.symbol = :stockSymbol")
+    boolean getStocksFromWatchList(
+            @Param("watchlistId") Long watchlistId,
+            @Param("userId") Long userId,
+            @Param("stockSymbol") String stockSymbol
+    );
 
 }
