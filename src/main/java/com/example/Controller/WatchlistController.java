92552ea519af85ac64cff3a5cbc69a979148c3f6
camb3la <59169021+camb3la@users.noreply.github.com>
2025-03-26 12:42:07 +0100

    Rimosso Prefisso Api da AuthController e WatchlistController

--- src/main/java/com/example/Controller/WatchlistController.java
+++ src/main/java/com/example/Controller/WatchlistController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/watchlists")
+@RequestMapping("/watchlists")
 @RequiredArgsConstructor
 public class WatchlistController {
 
